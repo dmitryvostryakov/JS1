079efb4843d2f4b2fd05a8a1de78ee9c35b2cf17
dmitryvostryakov <dmitryvostryakov@gmail.com>
2018-11-18 11:35:41 +0300

    небольшой апдейт для сдачи дз через pull

--- Lesson 1 - homework/homework.js
+++ Lesson 1 - homework/homework.js
@@ -25,3 +25,5 @@ alert(1000+"108");
 /* If async is present: The script is executed asynchronously with the rest of the page (the script will be executed while the page continues the parsing)
 If async is not present and defer is present: The script is executed when the page has finished parsing
 If neither async or defer is present: The script is fetched and executed immediately, before the browser continues parsing the page */
+
+//update
