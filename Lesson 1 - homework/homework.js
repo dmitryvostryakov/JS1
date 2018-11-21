12c0c80973e04cb8b4c312cbc1cf87dcee430c4d
dmitryvostryakov <dmitryvostryakov@gmail.com>
2018-11-21 19:54:59 +0300

    new homework - lesson 2 Done

--- Lesson 1 - homework/homework.js
+++ Lesson 1 - homework/homework.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 //1 - Задать температуру в градусах по Цельсию. Вывести в alert соответствующую температуру в градусах по Фаренгейту. Подсказка: расчёт идёт по формуле: Tf = (9 / 5) * Tc + 32, где Tf – температура по Фаренгейту, Tc – температура по Цельсию
 //2 - Работа с переменными.
@@ -27,3 +27,27 @@ If async is not present and defer is present: The script is executed when the pa
 If neither async or defer is present: The script is fetched and executed immediately, before the browser continues parsing the page */
 
 //update
+
+//3. Вывести в консоль значения выражений и объяснить почему получились такие значения используя комментарии к каждому выражению:
+console.log(10 + 10 + "10"); // 2010 т.к. при складывании числа и string возвращается string (If you add a number and a string, the result will be a string!)
+console.log(10 + "10" + 10); //101010 т.к. в данном случае происходит конкат двух чисел с разных сторон
+console.log(10 + 10 + +"10");// 30, т.к. в последнем случае унарный плюс преобразует стринг в число
+console.log(10 / -""); // -infitity, т.к. число в итоге делится на -0 из-за унарного минуса
+console.log(10 / +"2,5"); // NaN, т.к. в 2,5 используется "запятая" вместо "точки"
+
+//4. Напишите, являются ли данные имена переменных корректными:
+let mode = "normal"; // да
+let my_valu3 = 102; // да
+let 3my_value3 = "102"; // нет, т.к. нельзя начинать переменную с числа
+let __hello__ = "world"; // да
+let $$$ = "money"; // да
+let !0_world = true; // нет, т.к. нельзя начинать со спецсимвола
+
+
+
+
+
+
+
+
+
